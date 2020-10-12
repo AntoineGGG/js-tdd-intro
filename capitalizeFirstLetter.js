294f79241e44e29e9d9b349331574450795dcb08
AntoineGGG <a.gorenflot@gmx.fr>
2020-10-12 22:10:26 +0200

    Tous les test OK

--- capitalizeFirstLetter.js
+++ capitalizeFirstLetter.js
@@ -2,14 +2,24 @@
 const assert = require('assert');
 
 // WRITE THE ACTUAL FUNCTION HERE
-
+function capitalizeFirstLetter(input) {
+  text = input
+    .toLowerCase()
+    .split(' ')
+    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+  return text;
+}
 // Check that capitalizeFirst is a function
-assert.strictEqual(typeof capitalizeFirst, 'function');
+assert.strictEqual(typeof capitalizeFirstLetter, 'function');
 // Check that capitalizeFirst accepts one argument
-assert.strictEqual(capitalizeFirst.length, 1);
+assert.strictEqual(capitalizeFirstLetter.length, 1);
 // Check that capitalizeFirst transforms javaScript correctly
-assert.strictEqual(capitalizeFirst('i am learning TDD'), 'I Am Learning TDD');
+assert.strictEqual(
+  capitalizeFirstLetter('i am learning react'),
+  'I Am Learning React'
+);
 // Check that it works for a 1-character string
-assert.strictEqual(capitalizeFirst('z'), 'Z');
+assert.strictEqual(capitalizeFirstLetter('z'), 'Z');
 // Check that it works for an empty string
-assert.strictEqual(capitalizeFirst(''), '');
+assert.strictEqual(capitalizeFirstLetter(''), '');
